5c3331c2da17ef521717f7fd540712dfe13c670b
Szamurajpalanta <s_janos@outlook.com>
2022-04-26 21:59:28 +0200

    implement student search

--- backend/src/controller/student.controller.ts
+++ backend/src/controller/student.controller.ts
@@ -4,4 +4,18 @@ import { Controller } from "./base.controller";
 
 export class StudentController extends Controller {
     override repository = getRepository(Student);
+
+    search = async (req, res) => {
+        const query = req.query.search || '';
+
+        try {
+            const students = await this.repository.createQueryBuilder('student')
+                .where("title LIKE CONCAT('%', :param, '%')", { param: query })
+                .getMany();
+
+            res.json(students);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    };
 }
