5c8b53698acc072892d3eb3ef350ae7bb83560f5
Szamurajpalanta <s_janos@outlook.com>
2022-05-10 16:04:37 +0200

    get list of circles in statistics-list

--- src/app/statistics-list/statistics-list.component.ts
+++ src/app/statistics-list/statistics-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Enrollment } from '../models/enrollment';
+import { EnrollmentService } from '../services/enrollment.service';
 
 @Component({
   selector: 'app-statistics-list',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsListComponent implements OnInit {
 
-  constructor() { }
+  enrollments: Enrollment[] = [];
+  circles: string[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private enrollmentService: EnrollmentService
+  ) { }
+
+  async ngOnInit() {
+    try {
+      this.enrollments = await this.enrollmentService.getEnrollments();
+      this.enrollments.forEach(enrollment => {
+        if (!this.circles.includes(enrollment.student.circle)) {
+          this.circles.push(enrollment.student.circle);
+        }
+      });
+      console.log(this.circles)
+    } catch (err) {
+      console.error(err);
+    }
   }
 
 }
